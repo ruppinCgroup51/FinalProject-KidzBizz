ff6d06f0749f4ce2fd32d5d362e4f1e05392cf61
AviaPerelman <aviap1772@gmail.com>
2024-04-25 15:49:16 +0300

    AI Player start

---
+++ KidzBizzServer/KidzBizzServer/BL/AIPlayer.cs
@@ -0,0 +1,111 @@
+﻿//using System;
+
+//namespace KidzBizzServer.BL
+//{
+//    public enum PlayerType
+//    {
+//        Conservative,
+//        Adventurous,
+//        Balanced
+//    }
+
+//    public class AIPlayer
+//    {
+//        private PlayerType playerType;
+//        private List<bool> decisionHistory;
+        
+//        public AIPlayer(PlayerType type)
+//        {
+//            playerType = type;
+//            decisionHistory = new List<bool>();
+//        }
+
+//        // Method to set the player type randomly
+//        public PlayerType SetPlayerType()
+//        {
+//            Random random = new Random();
+//            int playerTypeIndex = random.Next(3);
+//            playerType = (PlayerType)playerTypeIndex;
+//            return playerType;
+//        }
+
+//        // Method to make a decision based on the player type
+//        public void MakeDecision(GameState gameState)
+//        {
+//            switch (playerType)
+//            {
+//                case PlayerType.Conservative:
+//                    MakeConservativeDecision(gameState);
+//                    break;
+//                case PlayerType.Adventurous:
+//                    MakeAdventurousDecision(gameState);
+//                    break;
+//                case PlayerType.Balanced:
+//                    MakeBalancedDecision(gameState);
+//                    break;
+//                default:
+//                    Console.WriteLine("Invalid player type.");
+//                    break;
+//            }
+//        }
+
+//        private void MakeConservativeDecision(GameState gameState)
+//        {
+//            // Implement decision-making logic for a conservative player
+//            Random random = new Random();
+//            bool buyAsset = random.Next(4) == 0; // 25% chance of buying asset
+//            decisionHistory.Add(buyAsset);
+//            Console.WriteLine("Conservative player decides to " + (buyAsset ? "save money." : "buy an asset."));
+//        }
+
+//        private void MakeAdventurousDecision(GameState gameState)
+//        {
+//            // Implement decision-making logic for an adventurous player
+//            Random random = new Random();
+//            bool buyAsset = random.Next(2) == 0; // 50% chance of buying asset
+//            decisionHistory.Add(buyAsset);
+//            Console.WriteLine("Adventurous player decides to " + (buyAsset ? "buy an asset." : "save money."));
+//        }
+
+//        private void MakeBalancedDecision(GameState gameState)
+//        {
+//            // Implement decision-making logic for a balanced player
+//            Random random = new Random();
+//            bool buyAsset = random.Next(3) == 0; // 33% chance of buying asset
+//            decisionHistory.Add(buyAsset);
+//            Console.WriteLine("Balanced player decides to " + (buyAsset ? "buy an asset." : "save money."));
+//        }
+
+
+//        // Method to get the last n decisions
+//        public List<bool> GetLastNDecisions(int n)
+//        {
+//            if (decisionHistory.Count <= n)
+//            {
+//                return decisionHistory;
+//            }
+//            else
+//            {
+//                int startIndex = decisionHistory.Count - n;
+//                return decisionHistory.GetRange(startIndex, n);
+//            }
+//        }
+
+//        public void PrintDecisionHistory()
+//        {
+//            Console.WriteLine("Decision history for " + playerType + " player:");
+//            foreach (bool decision in decisionHistory)
+//            {
+//                Console.WriteLine(decision ? "Buy asset" : "Save money");
+//            }
+//        }   
+
+      
+//    }
+
+
+
+//}
+
+
+//}
---
+++ KidzBizzServer/KidzBizzServer/BL/GameState.cs
@@ -0,0 +1,59 @@
+﻿//namespace KidzBizzServer.BL
+//{
+//    public class GameState
+//    {
+//        // Define properties to represent the current state of the game
+//        public int PlayerMoney { get; set; }
+//        public int OpponentMoney { get; set; }
+//        public int PlayerAssets { get; set; }
+//        public int OpponentAssets { get; set; }
+//        // Add more properties as needed
+
+//        // Constructor
+//        public GameState(int playerMoney, int opponentMoney, int playerAssets, int opponentAssets)
+//        {
+//            PlayerMoney = playerMoney;
+//            OpponentMoney = opponentMoney;
+//            PlayerAssets = playerAssets;
+//            OpponentAssets = opponentAssets;
+//        }
+//    }
+
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // Create an instance of the game state
+
+//            GameState gameState = new GameState(//need to insert the data from the game )
+
+//            // Create AI players of different types
+//            AIPlayer conservativePlayer = new AIPlayer(PlayerType.Conservative);
+//            AIPlayer adventurousPlayer = new AIPlayer(PlayerType.Adventurous);
+//            AIPlayer balancedPlayer = new AIPlayer(PlayerType.Balanced);
+
+//            // Make decisions for each player
+//            conservativePlayer.MakeDecision(gameState);
+//            adventurousPlayer.MakeDecision(gameState);
+//            balancedPlayer.MakeDecision(gameState);
+
+
+
+//            //// Display decision history for each player
+//            //Console.WriteLine("Conservative player decision history:");
+//            //foreach (bool decision in conservativePlayer.DecisionHistory)
+//            //{
+//            //    Console.WriteLine(decision ? "Save money" : "Buy an asset");
+//            //}
+            
+//            //Console.WriteLine("Adventurous player decision history:");
+//            //foreach (bool decision in adventurousPlayer.DecisionHistory)
+//            //{
+//            //    Console.WriteLine(decision ? "Save money" : "Buy an asset");
+//            //}
+//            //Console.WriteLine()
+//        }
+//    }
+
+//}
