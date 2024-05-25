d49c43a36faf13804ad33d90bd08febce06c64da
AviaPerelman <aviap1772@gmail.com>
2024-05-25 17:06:02 +0300

    Update main.jsx

--- KidzBizzReact/kidzbiz-react/src/main.jsx
+++ KidzBizzReact/kidzbiz-react/src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
-);
+  </HashRouter>);
