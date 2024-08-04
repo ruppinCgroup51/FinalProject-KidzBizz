e68e83b6f4d92821410320b5ed3ce189ee5ebbae
AviaPerelman <aviap1772@gmail.com>
2024-08-04 21:23:38 +0300

    Reapply "smart elment"
    This reverts commit 628250a202ffd1d6c9e260753e5d840cf12ee9be.

--- KidzBizzServer/KidzBizzServer/BL/AIPlayer.cs
+++ KidzBizzServer/KidzBizzServer/BL/AIPlayer.cs
@@ -1,117 +1,5 @@
-﻿//using System;
-
-//namespace KidzBizzServer.BL
-//{
-//    public enum PlayerType
-//    {
-//        Conservative,
-//        Adventurous,
-//        Balanced
-//    }
-
-//    public class AIPlayer
-//    {
-//        private PlayerType playerType;
-//        private List<bool> decisionHistory;
-
-//        public AIPlayer(PlayerType type)
-//        {
-//            playerType = type;
-//            decisionHistory = new List<bool>();
-//        }
-
-//        // Method to set the player type randomly
-//        public PlayerType SetPlayerType()
-//        {
-//            Random random = new Random();
-//            int playerTypeIndex = random.Next(3);
-//            playerType = (PlayerType)playerTypeIndex;
-//            return playerType;
-//        }
-
-//        // Method to make a decision based on the player type
-//        public void MakeDecision(GameState gameState)
-//        {
-//            switch (playerType)
-//            {
-//                case PlayerType.Conservative:
-//                    MakeConservativeDecision(gameState);
-//                    break;
-//                case PlayerType.Adventurous:
-//                    MakeAdventurousDecision(gameState);
-//                    break;
-//                case PlayerType.Balanced:
-//                    MakeBalancedDecision(gameState);
-//                    break;
-//                default:
-//                    Console.WriteLine("Invalid player type.");
-//                    break;
-//            }
-//        }
-
-//private void MakeConservativeDecision(GameState gameState)
-//{
-//    Implement decision-making logic for a conservative player
-
-//   Random random = new Random();
-//    bool buyAsset = random.Next(4) == 0; // 25% chance of buying asset
-//    decisionHistory.Add(buyAsset);
-//    Console.WriteLine("Conservative player decides to " + (buyAsset ? "save money." : "buy an asset."));
-//}
-
-//        private void MakeAdventurousDecision(GameState gameState)
-//        {
-//            // Implement decision-making logic for an adventurous player
-//            Random random = new Random();
-//            bool buyAsset = random.Next(2) == 0; // 50% chance of buying asset
-//            decisionHistory.Add(buyAsset);
-//            Console.WriteLine("Adventurous player decides to " + (buyAsset ? "buy an asset." : "save money."));
-//        }
-
-//        private void MakeBalancedDecision(GameState gameState)
-//        {
-//            // Implement decision-making logic for a balanced player
-//            Random random = new Random();
-//            bool buyAsset = random.Next(3) == 0; // 33% chance of buying asset
-//            decisionHistory.Add(buyAsset);
-//            Console.WriteLine("Balanced player decides to " + (buyAsset ? "buy an asset." : "save money."));
-//        }
-
-
-//        // Method to get the last n decisions
-//        public List<bool> GetLastNDecisions(int n)
-//        {
-//            if (decisionHistory.Count <= n)
-//            {
-//                return decisionHistory;
-//            }
-//            else
-//            {
-//                int startIndex = decisionHistory.Count - n;
-//                return decisionHistory.GetRange(startIndex, n);
-//            }
-//        }
-
-//        public void PrintDecisionHistory()
-//        {
-//            Console.WriteLine("Decision history for " + playerType + " player:");
-//            foreach (bool decision in decisionHistory)
-//            {
-//                Console.WriteLine(decision ? "Buy asset" : "Save money");
-//            }
-//        }   
-
-
-//    }
-
-
-
-//}
-
-
-//}
-using System;
-using static System.Net.WebRequestMethods;
+﻿using System;
+using System.Collections.Generic;
 
 namespace KidzBizzServer.BL
 {
@@ -122,15 +10,17 @@ namespace KidzBizzServer.BL
         Adventurous,   // שחקן הרפתקן
         Balanced       // שחקן מאוזן
     }
+
     // מחלקת AIPlayer מייצגת שחקן אוטונומי במערכת
     public class AIPlayer : Player
     {
         // תכונה ציבורית לקבלת סוג השחקן
         public PlayerType PlayerType { get; private set; }
+        private static Random random = new Random();
 
         public AIPlayer()
         {
-
+            PlayerType = (PlayerType)random.Next(0, 3); // הגרלת סוג השחקן
         }
 
         public class GameState
@@ -139,7 +29,7 @@ namespace KidzBizzServer.BL
             public double CurrentRentPotential { get; set; }
         }
 
-        public AIPlayer(int playerId, User user, int currentPosition, double currentBalance, string playerStatus, int lastDiceResult, List<Property> properties, int dice1 , int dice2 , PlayerType type)
+        public AIPlayer(int playerId, User user, int currentPosition, double currentBalance, string playerStatus, int lastDiceResult, List<Property> properties, int dice1, int dice2, PlayerType type)
            : base(playerId, user ?? new User
            {
                Gender = "Not specified",
@@ -148,12 +38,12 @@ namespace KidzBizzServer.BL
                Username = "AIPlayer",
                FirstName = "AI",
                AvatarPicture = "https://robohash.org/avatar1"
-           }, currentPosition, currentBalance, playerStatus, lastDiceResult, properties , dice1 , dice2)
+           }, currentPosition, currentBalance, playerStatus, lastDiceResult, properties, dice1, dice2)
         {
             PlayerType = type;
         }
 
-        public AIPlayer (PlayerType type)
+        public AIPlayer(PlayerType type)
         {
             PlayerType = type;
         }
@@ -242,6 +132,7 @@ namespace KidzBizzServer.BL
         }
     }
 }
+
 namespace KidzBizzServer.BL
 {
     // מחלקת PlayerActionData מייצגת נתונים היסטוריים של פעולות שחקן
